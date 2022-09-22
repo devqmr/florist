19c339c94678208629d9b1035fb9d6985cd99164
devqmr <devqmr@gmail.com>
2022-09-22 13:15:47 +0300

    Handel any error happen while fetch flowers from server-side

--- lib/providers/flowers.dart
+++ lib/providers/flowers.dart
@@ -4,27 +4,32 @@ import 'package:florist/providers/flower.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/general_exception.dart';
 import '../my_constant.dart';
 
 class Flowers with ChangeNotifier {
   List<Flower> flowers = [];
 
   Future<void> fetchFlowers() async {
-    final url = Uri.https(MyConstant.FIREBASE_RTDB_URL, "/flowers.json");
-    final response = await http.get(url);
+    try {
+      final url = Uri.https(MyConstant.FIREBASE_RTDB_URL, "/flowers.json");
+      final response = await http.get(url);
 
-    await Future.delayed(const Duration(seconds: 3));
+      await Future.delayed(const Duration(seconds: 5));
 
-    Map<String, dynamic> mm = jsonDecode(response.body);
-    mm.forEach((key, value) {
-      flowers.add(Flower(
-          id: key,
-          title: value['title'],
-          description: value['description'],
-          imageUrl: value['imageUrl'],
-          price: value['price'],
-          isFavorite: value['isFavorite']));
-    });
+      Map<String, dynamic> mm = jsonDecode(response.body);
+      mm.forEach((key, value) {
+        flowers.add(Flower(
+            id: key,
+            title: value['title'],
+            description: value['description'],
+            imageUrl: value['imageUrl'],
+            price: value['price'],
+            isFavorite: value['isFavorite']));
+      });
+    } catch (e) {
+      throw (GeneralException("Error!, We can not load flowers..."));
+    }
 
     notifyListeners();
   }
--- lib/screens/florist_collection_screen.dart
+++ lib/screens/florist_collection_screen.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:florist/providers/flower.dart';
 import 'package:florist/providers/flowers.dart';
 import 'package:florist/widgets/flower_item.dart';
@@ -26,14 +27,33 @@ class _FloristCollectionScreenState extends State<FloristCollectionScreen> {
       setState(() => _isLoading = true);
 
       _flowersProvider = Provider.of<Flowers>(context);
-      _flowersProvider.fetchFlowers().then((_) {
+
+      _flowersProvider.fetchFlowers().then((value) {
         setState(() {
           _isLoading = false;
         });
+      }).catchError((e) {
+        setState(() {
+          _isLoading = false;
+        });
+        showErrorMessage(e.toString());
       });
+
       flowersList = _flowersProvider.flowers;
       _needToInit = false;
     }
+  }
+
+  void showErrorMessage(String errorMessage) {
+    final snackBar = SnackBar(
+        elevation: 0,
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: Colors.transparent,
+        content: AwesomeSnackbarContent(
+            title: "Error!",
+            message: errorMessage,
+            contentType: ContentType.failure));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
   @override
@@ -46,12 +66,13 @@ class _FloristCollectionScreenState extends State<FloristCollectionScreen> {
       ),
       body: Container(
         child: _isLoading
-            ? Center(
+            ? const Center(
                 child: CircularProgressIndicator(),
               )
             : GridView.builder(
                 padding: const EdgeInsets.all(20),
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                gridDelegate:
+                    const SliverGridDelegateWithMaxCrossAxisExtent(
                   maxCrossAxisExtent: 200,
                   childAspectRatio: 4 / 5,
                   mainAxisSpacing: 10,
