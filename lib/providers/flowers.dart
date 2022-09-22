65c3f7a235c58902c9c156d79de08f43d012f1f7
devqmr <devqmr@gmail.com>
2022-09-22 11:32:04 +0300

    Fetch flowers data from server side [Firebase RealTime DataBase] using REST api.

--- lib/providers/flowers.dart
+++ lib/providers/flowers.dart
@@ -1,53 +1,29 @@
+import 'dart:convert';
+
 import 'package:florist/providers/flower.dart';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+import '../my_constant.dart';
 
 class Flowers with ChangeNotifier {
   List<Flower> flowers = [];
 
   void fetchFlowers() {
-    flowers = [
-      Flower(
-          id: 'id001',
-          title: "Flower 001",
-          description: "Nice flower",
-          imageUrl:
-              "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/diy-paper-flowers-1582662788.jpg",
-          price: 3.4,
-          isFavorite: false),
-      Flower(
-          id: 'id002',
-          title: "Flower 002",
-          description: "Nice flower",
-          imageUrl:
-              "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/diy-paper-flowers-1582662788.jpg",
-          price: 3.4,
-          isFavorite: false),
-      Flower(
-          id: 'id003',
-          title: "Flower 003",
-          description: "Nice flower",
-          imageUrl:
-              "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/diy-paper-flowers-1582662788.jpg",
-          price: 3.4,
-          isFavorite: false),
-      Flower(
-          id: 'id004',
-          title: "Flower 004",
-          description: "Nice flower",
-          imageUrl:
-              "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/diy-paper-flowers-1582662788.jpg",
-          price: 3.4,
-          isFavorite: false),
-      Flower(
-          id: 'id005',
-          title: "Flower 005",
-          description: "Nice flower",
-          imageUrl:
-              "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/diy-paper-flowers-1582662788.jpg",
-          price: 3.4,
-          isFavorite: false),
-    ];
+    final url = Uri.https(MyConstant.FIREBASE_RTDB_URL, "/flowers.json");
+    http.get(url).then((respone) {
+      Map<String, dynamic> mm = jsonDecode(respone.body);
+      mm.forEach((key, value) {
+        flowers.add(Flower(
+            id: key,
+            title: value['title'],
+            description: value['description'],
+            imageUrl: value['imageUrl'],
+            price: value['price'],
+            isFavorite: value['isFavorite']));
+      });
 
-    notifyListeners();
+      notifyListeners();
+    });
   }
 }
