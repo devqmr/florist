ad56cce66e45afe0d86f269ffec83e8fafdce421
devqmr <devqmr@gmail.com>
2022-09-22 16:57:55 +0300

    Show the favorite products list

--- lib/screens/fav_flowers_screen.dart
+++ lib/screens/fav_flowers_screen.dart
@@ -1,4 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'package:florist/providers/flowers.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/flower_item.dart';
 
 class FavFlowersScreen extends StatelessWidget {
   static const screenName = '/fav_flowers';
@@ -7,10 +11,29 @@ class FavFlowersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('FavFlowerScreen'),
-      ),
+    final favFlowersList = Provider.of<Flowers>(context).favFlowersList;
+
+    return Center(
+      child: GridView.builder(
+          padding: const EdgeInsets.all(20),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 4 / 5,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 15,
+          ),
+          itemCount: favFlowersList.length,
+          itemBuilder: (context, index) {
+            return ChangeNotifierProvider.value(
+              value: favFlowersList[index],
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.lightGreenAccent,
+                ),
+                child: FlowerItem(),
+              ),
+            );
+          }),
     );
   }
 }
