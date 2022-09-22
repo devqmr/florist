e46c909cbd8678dcc2aac19038f6684584aed239
devqmr <devqmr@gmail.com>
2022-09-22 16:27:04 +0300

    Update flower isFavorite on server-side

--- lib/providers/flower.dart
+++ lib/providers/flower.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:florist/my_constant.dart';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Flower with ChangeNotifier {
   final String id;
@@ -17,8 +21,31 @@ class Flower with ChangeNotifier {
     required this.isFavorite,
   });
 
-  void toggleFavorite() {
-    isFavorite = !isFavorite;
+  void _setFavoriteValue(bool fav) {
+    isFavorite = fav;
     notifyListeners();
+  }
+
+  void toggleFavorite() async {
+    final oldFavStatus = isFavorite;
+    _setFavoriteValue(!isFavorite);
+
+    try {
+      final url = Uri.https(MyConstant.FIREBASE_RTDB_URL, '/flowers/$id.json');
+      print("url > $url");
+
+      final response = await http.patch(
+        url,
+        body: json.encode({'isFavorite': isFavorite}),
+      );
+
+      print("response.statusCode > ${response.statusCode}");
+      if (response.statusCode >= 400) {
+        _setFavoriteValue(oldFavStatus);
+      }
+    } catch (e) {
+      print(e);
+      _setFavoriteValue(oldFavStatus);
+    }
   }
 }
