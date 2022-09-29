87bf291b266453c207537ba4c9bece5685c57fae
devqmr <devqmr@gmail.com>
2022-09-29 12:50:10 +0300

    Make the cart items as Expanded

--- lib/screens/cart_screens.dart
+++ lib/screens/cart_screens.dart
@@ -29,38 +29,35 @@ class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     final _cartProvider = Provider.of<Cart>(context);
-    return Container(
-      child: Column(
-        children: [
-          Card(
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-              child: Row(
-                children: [
-                  Expanded(child: Text('Total')),
-                  Text('\$ ${_cartProvider.totalAmount.toStringAsFixed(2)}'),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text('Order Now'),
-                  )
-                ],
-              ),
+    return Column(
+      children: [
+        Card(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+            child: Row(
+              children: [
+                const Expanded(child: Text('Total')),
+                Text('\$ ${_cartProvider.totalAmount.toStringAsFixed(2)}'),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Order Now'),
+                )
+              ],
             ),
           ),
-          Container(
-            height: 500,
-            child: ListView.builder(
-              itemBuilder: (ctx, i) {
-                return ChangeNotifierProvider.value(
-                  value: _cartProvider.cartItems[i],
-                  child: CartItem(),
-                );
-              },
-              itemCount: _cartProvider.cartItems.length,
-            ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (ctx, i) {
+              return ChangeNotifierProvider.value(
+                value: _cartProvider.cartItems[i],
+                child: const CartItem(),
+              );
+            },
+            itemCount: _cartProvider.cartItems.length,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
