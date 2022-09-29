e5c8ecb0e1d6c1e1dbdd1b0d85a53220b3171135
devqmr <devqmr@gmail.com>
2022-09-29 16:57:01 +0300

    Add some items to MoreScreen

--- lib/screens/more_screen.dart
+++ lib/screens/more_screen.dart
@@ -7,6 +7,88 @@ class MoreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 64,
+        horizontal: 32,
+      ),
+      alignment: Alignment.topCenter,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          MoreScreenItem(
+            title: "Your orders",
+            icon: Icons.add_card,
+            fun: () => ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text(
+                  "Open Your orders screen",
+                ),
+              ),
+            ),
+          ),
+          MoreScreenItem(
+            title: "Manage Flowers",
+            icon: Icons.edit_note,
+            fun: () => ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text(
+                  "Open Manage Flowers screen",
+                ),
+              ),
+            ),
+          ),
+          MoreScreenItem(
+            title: "About",
+            icon: Icons.info,
+            fun: () => ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text(
+                  "Open About Screen",
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MoreScreenItem extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final Function fun;
+
+  const MoreScreenItem(
+      {Key? key, required this.title, required this.icon, required this.fun})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        fun();
+      },
+      child: Container(
+        color: Colors.amberAccent,
+        padding: EdgeInsets.all(16),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              icon,
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            Text(
+              title,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
