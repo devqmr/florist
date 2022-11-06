88e809b2e191b7b03f368edd5295182d20df78ef
devqmr <devqmr@gmail.com>
2022-11-06 12:34:59 +0200

    Move logic to separate function

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,13 +1,21 @@
+import 'dart:io';
+
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/auth.dart';
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends StatefulWidget {
   static const screenName = '/auth';
 
   const AuthScreen({Key? key}) : super(key: key);
 
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     final authProvider = Provider.of<Auth>(context);
@@ -39,15 +47,12 @@ class AuthScreen extends StatelessWidget {
               TextField(
                 decoration:
                     const InputDecoration(hintText: "Confirm Your Password"),
-                controller: confirmPasswordController,
+                controller: confirmPasswordController..text = '123456',
                 keyboardType: TextInputType.visiblePassword,
               ),
               ElevatedButton(
                 onPressed: () {
-                  authProvider.signUp(
-                    emailController.text,
-                    passwordController.text,
-                  );
+                  signUp(authProvider, emailController, passwordController);
                 },
                 child: Text(
                   'Signup',
@@ -64,7 +69,15 @@ class AuthScreen extends StatelessWidget {
                   'SignIn',
                 ),
               ),
-              authProvider.isAuthenticated()
+              ElevatedButton(
+                onPressed: () {
+                  authProvider.fetchAuthData();
+                },
+                child: const Text(
+                  'Check Sign-In from local data',
+                ),
+              ),
+              authProvider.isAuthenticated
                   ? Text('Authenticated')
                   : Text('Not Authenticated')
             ],
@@ -72,5 +85,37 @@ class AuthScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void signUp(Auth authProvider, TextEditingController emailController,
+      TextEditingController passwordController) async{
+    try {
+      await authProvider.signUp(
+        emailController.text,
+        passwordController.text,
+      );
+    } on HttpException catch (error) {
+      if (error.message.contains('EMAIL_EXISTS')) {
+        print(
+            'This email is already used please used another one, or go to sign in ');
+        showErrorMessage(
+            'This email is already used');
+      }
+    } catch (e) {
+      print(e);
+      print('Try again');
+    }
+  }
+
+  void showErrorMessage(String errorMessage) {
+    final snackBar = SnackBar(
+        elevation: 0,
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: Colors.transparent,
+        content: AwesomeSnackbarContent(
+            title: "Error!",
+            message: errorMessage,
+            contentType: ContentType.failure));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
