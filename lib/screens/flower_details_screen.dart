c9a8b3e18fae3ad25cd5bf1e5a39a7589c4fbce1
devqmr <devqmr@gmail.com>
2022-11-23 16:20:37 +0200

    Update the flower_details_screen.dart UI

--- lib/screens/flower_details_screen.dart
+++ lib/screens/flower_details_screen.dart
@@ -2,6 +2,8 @@ import 'package:florist/providers/flowers.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../app_style.dart';
+
 class FlowerDetailsScreen extends StatelessWidget {
   static const screenName = "/flower_details";
 
@@ -14,24 +16,53 @@ class FlowerDetailsScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(flower.title),
+        title: Text(
+          flower.title,
+          style: AppStyle.appFontBold.copyWith(fontSize: 30),
+        ),
       ),
       body: Center(
           child: Column(
         children: [
-          Image.network(
-            flower.imageUrl,
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: 300,
+          Hero(
+            tag: flower.id,
+            child: Image.network(
+              flower.imageUrl,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 300,
+            ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(flower.description),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text("\$ ${flower.price}"),
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 40),
+                Text(
+                  flower.title,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      color: Colors.purple),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(flower.description,
+                      style:
+                          const TextStyle(fontSize: 15, color: Colors.purple)),
+                ),
+                const SizedBox(height: 20),
+                Align(
+                  child: Text(
+                    "\$ ${flower.price}",
+                    style: AppStyle.appFontBold.copyWith(fontSize: 30),
+                  ),
+                  alignment: Alignment.center,
+                )
+              ],
+            ),
           )
         ],
       )),
